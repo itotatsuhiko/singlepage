f3c84603d5bfba504d5a607c9631d63a694a5ee8
itotatsuhiko <37993270+itotatsuhiko@users.noreply.github.com>
2018-04-26 18:12:05 +0900

    [update] url:/layer と/users/moreの追加
    [update] url:/layer と/users/moreの追加

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,5 +12,16 @@ router.get('/users', function(req, res, next) {
   res.render('users/index', { title: 'Express' });
 });
 
+/* GET users listing. */
+router.get('/users/more', function(req, res, next) {
+  //res.send('respond with a resource');
+  res.render('users/more/index', { title: 'more' });
+});
+
+/* GET layer listing. */
+router.get('/layer',function(req,res,next) {
+  //res.send('respond with a resource');
+  res.render('layer/index', { title: 'Open Layer'})
+});
 
 module.exports = router;
