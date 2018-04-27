c1423ba9de094a9869d7c67d2c61c115e934b05a
itotatsuhiko <37993270+itotatsuhiko@users.noreply.github.com>
2018-04-27 18:30:15 +0900

    [update]画面遷移実装

--- public/javascripts/users/index.js
+++ public/javascripts/users/index.js
@@ -3,7 +3,7 @@ window.BaseApp.Users || (window.BaseApp.Users = {});
 
 
 var userList = 
-BaseApp.Users.UserList = function(){
+BaseApp.Users.UserList = function() {
     var $workArea = $('#container');
     this.$btnGetUserList = $workArea.find('.btn_search');
     this.$btnGetMore = $workArea.find('.btn_more');
@@ -12,24 +12,23 @@ BaseApp.Users.UserList = function(){
 userList.prototype.init = function () {
     console.log('init');
     this.$btnGetUserList.click(this.onClickGetUserListBtn.bind(this));
-    this.$btnGetMore.click(this.onClickGetMoreBtn.bind(this));
-    console.log('this');
-    console.log(this);
 }
 //ボタン押下時の処理
-userList.prototype.onClickGetUserListBtn = function(){
+userList.prototype.onClickGetUserListBtn = function() {
     console.log('click');
     var apiService = new BaseApp.Users.ApiService(this);
     apiService.getJsonData(this.$userTable);
 }
-userList.prototype.onClickGetMoreBtn = function(){
+//詳細画面遷移
+$(document).on('click','.btn_more',function(){
     console.log('moreBtnclick');
     window.location.href = '/users/more/';
-}
-
-
-userList.prototype.makeTable = function(payload){
+});
+//テーブルを作成
+userList.prototype.makeTable = function(payload) {
     var data = payload.data;
+    console.log('payload.data');
+    console.log(payload.data);
     $.extend( $.fn.dataTable.defaults, {
         language: { url: "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json" }
     });
@@ -44,33 +43,38 @@ userList.prototype.makeTable = function(payload){
             { data: "age" } ,
             { data: "gender"},
             { data : "btn", 
-                    orderable: false, 
+                    orderable: true, 
                     title : 'btn' , 
-                    'defaultContent': '<button class=btn btn-default btn_more>More</button>'
+                    'defaultContent': '<button class="btn btn-default btn_more">More</button>'
             },
         ]
     });
 }
 
 //javascriptのエントリポイント
-$(document).ready(function(){
+$(document).ready(function() {
     console.log('hello');
     var userList = new BaseApp.Users.UserList();
     userList.init();
+
 });
 
 //api処理用のオブジェクト(クラス)
 var apiService = BaseApp.Users.ApiService = function (context) {
     this.context = context;
 }
-apiService.prototype.init = function(){
+apiService.prototype.init = function() {
 }
-apiService.prototype.getJsonData = function(){
-    $.ajax( { url: "http://localhost:3000/api/users",
+apiService.prototype.getJsonData = function() {
+    var json = 'http://localhost:3000/api/users'
+    $.ajax( { 
+        url: json,
         status: "true",
         dataSrc: "data" 
-     }).done( function (data, textStatus, jqXHR) {
-         //文字'歳'の付与
+    
+     })
+     .done( function (data, textStatus, jqXHR) {
+       //文字'歳'の付与
         var changeData;
         for (var i = 0; i < data.data.length; i++) {
             changeData = data.data[i].age + '歳';
@@ -81,13 +85,11 @@ apiService.prototype.getJsonData = function(){
         for (var i =0; i < data.data.length; i++) {
             data.data[i].gender = addData;
         }
-        // console.log(data.data);
-        // console.log(data);
+
         this.context.makeTable(data);
     }.bind(this)) 
     .fail( function (jqXHR, textStatus, error) {
         console.log('error');
     });
 }
-
 
