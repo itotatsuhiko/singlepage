a4105ed0359e45a5a0f9a44810cf1b70ccfd9894
itotatsuhiko <37993270+itotatsuhiko@users.noreply.github.com>
2018-04-27 18:19:51 +0900

    [update] JsonデータURL追加

--- index.js
+++ index.js
@@ -1,62 +1,16 @@
-window.BaseApp || (window.BaseApp = {});
-window.BaseApp.Users || (window.BaseApp.Users = {});
+var express = require('express');
+var router = express.Router();
 
-
-var userList = 
-BaseApp.Users.UserList = function(){
-    var $workArea = $('#container');
-    this.$btnGetUserList = $workArea.find('.btn_search');
-    this.$userTable = $workArea.find('#user_list_table');
-}
-userList.prototype.init = function () {
-    console.log('init');
-    this.$btnGetUserList.click(this.onClickGetUserListBtn.bind(this));
-}
-
-userList.prototype.onClickGetUserListBtn = function(){
-    console.log('click');
-    var apiService = new BaseApp.Users.ApiService(this);
-    apiService.getUserList().bind(this);
-}
-userList.prototype.renderTableRow = function(payload) {
-    console.log('render');
-    var data = payload.data;
-    console.log(data);
-    this.$userTable.find('tbody').empty();
+var userController = require('./user.controllers');
+var genderController = require('./gender.controllers');
+/* GET users list */
+router.get('/users', function(req, res, next) {
     
-}
-//javascriptのエントリポイント
-$(document).ready(function(){
-    console.log('hello');
-    var userList = new BaseApp.Users.UserList();
-    userList.init();
+    userController.getUserList(req, res, next);
 });
-
-//api処理用のオブジェクト(クラス)
-var apiService = BaseApp.Users.ApiService = function (context) {
-    this.context = context;
-}
-apiService.prototype.init = function(){
-}
-apiService.prototype.getUserList = function(){
-    console.log(this.$userTable);
-//    $("#user_list_table").DataTable(
-    this.$userTable.DataTable(
-        $.ajax({
-            url : 'http://localhost:3000/api/users',
-            datatype : 'json',
-            columns : [
-                { data : "user_id" },
-                { data : "name" },
-                { data : "age" }
-            ]
-        }).done( function (data, textStatus, jqXHR) {
-            console.log(data);
-            console.log("this:");
-            console.log(this);
-            this.context.renderTableRow(data);
-        }.bind(this)) 
-        .fail( function (jqXHR, textStatus, error) {
-            console.log('error');
-        })
-    )};
+/* GET users list */
+router.get('/gender', function(req, res, next) {
+    
+    genderController.getgender(req, res, next);
+});
+module.exports = router;
